16f78d7c6d4177636c99497b920c4c7ead6a7357
iyisakuma <yujisakuma224@gmail.com>
2025-03-17 19:53:16 -0300

    Meu primeiro commit

---
+++ src/main/java/br/edu/ufpel/rokamokaServer/RokamokaServerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufpel.rokamokaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokamokaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokamokaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufpel/rokamokaServer/RokamokaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufpel.rokamokaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RokamokaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
