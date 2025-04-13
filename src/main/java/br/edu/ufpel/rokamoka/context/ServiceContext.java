bea02044604569bccaf64f0ce99f9d12f7289570
MauricioMucci <mauricio_mucci@hotmail.com>
2025-04-12 22:42:28 -0300

    feat: add ServiceContext class for managing request context and execution tracking
    Signed-off-by: MauricioMucci <mauricio_mucci@hotmail.com>

---
+++ src/main/java/br/edu/ufpel/rokamoka/context/ServiceContext.java
@@ -0,0 +1,82 @@
+package br.edu.ufpel.rokamoka.context;
+
+import br.edu.ufpel.rokamoka.security.UserAuthenticated;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
+
+/**
+ * @author mauriciomucci
+ */
+@Data
+@Slf4j
+public class ServiceContext {
+
+    private static String USERNAME = "username";
+    private static String EXECUTION_UUID = "executionUUID";
+
+    private static final ThreadLocal<ServiceContext> threadLocal = new ThreadLocal<>();
+
+    // for thread timing
+    private final long startTime = System.currentTimeMillis();
+    private long endTime;
+    private long elapsedTime;
+
+    // for request context
+    private UserAuthenticated user;
+    private HttpStatus httpStatus;
+    private String executionUUID;
+    private Exception exception;
+
+    private ServiceContext(String executionUUID) {
+        if (StringUtils.isBlank(executionUUID)) {
+            executionUUID = java.util.UUID.randomUUID().toString();
+        }
+
+        this.executionUUID = executionUUID;
+        this.httpStatus = HttpStatus.OK;
+
+        MDC.put(EXECUTION_UUID, this.executionUUID);
+        MDC.remove(USERNAME);
+    }
+
+    public static synchronized ServiceContext getContext() {
+        ServiceContext ctx = threadLocal.get();
+        if (ctx == null) {
+            ctx = newContext();
+            log.debug("Novo contexto: [{}]", ctx.executionUUID);
+            return ctx;
+        }
+
+        log.debug("Retornando contexto: [{}]", ctx.executionUUID);
+        return ctx;
+    }
+
+    public static ServiceContext newContext() {return newContext(java.util.UUID.randomUUID().toString());}
+
+    private static ServiceContext newContext(String executionUUID) {
+        ServiceContext ctx = new ServiceContext(executionUUID);
+        threadLocal.set(ctx);
+        return ctx;
+    }
+
+    public static synchronized void clearContext() {
+        if (threadLocal.get() != null) {
+            log.debug("Limpando contexto: [{}]", threadLocal.get().executionUUID);
+        }
+        threadLocal.remove();
+        MDC.clear();
+    }
+
+    public void setUser(UserAuthenticated user) {
+        this.user = user;
+        MDC.put(USERNAME, user.getUsername());
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+        this.elapsedTime = this.endTime - startTime;
+    }
+}
