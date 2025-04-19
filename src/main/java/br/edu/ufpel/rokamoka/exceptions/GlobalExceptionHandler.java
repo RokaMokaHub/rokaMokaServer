9b352c643ebf5cf1af1c94384e069a3cac0faa54
MauricioMucci <mauricio_mucci@hotmail.com>
2025-04-19 17:54:44 -0300

    feat(exceptions): Add handling for ConstraintViolationException in GlobalExceptionHandler
    Signed-off-by: MauricioMucci <mauricio_mucci@hotmail.com>

--- src/main/java/br/edu/ufpel/rokamoka/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/edu/ufpel/rokamoka/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package br.edu.ufpel.rokamoka.exceptions;
 
+import br.edu.ufpel.rokamoka.context.ApiResponseWrapper;
+import br.edu.ufpel.rokamoka.wrapper.RokaMokaController;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import br.edu.ufpel.rokamoka.context.ApiResponseWrapper;
-import br.edu.ufpel.rokamoka.wrapper.RokaMokaController;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author mauriciomucci
@@ -17,7 +16,10 @@ import lombok.extern.slf4j.Slf4j;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends RokaMokaController {
 
-    @ExceptionHandler(value = { RokaMokaContentDuplicatedException.class })
+    @ExceptionHandler(value = {
+            RokaMokaContentDuplicatedException.class,
+            ConstraintViolationException.class
+    })
     public ResponseEntity<ApiResponseWrapper<Void>> handleBadRequest(Exception ex) {
         return error(ex, HttpStatus.BAD_REQUEST);
     }
