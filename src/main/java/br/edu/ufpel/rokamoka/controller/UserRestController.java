b4034c711f05bd5a9c4ad52435f08b849cb30581
MauricioMucci <mauricio_mucci@hotmail.com>
2025-04-19 18:31:31 -0300

    style(controller): Reorganize imports and format comments
    Signed-off-by: MauricioMucci <mauricio_mucci@hotmail.com>

--- src/main/java/br/edu/ufpel/rokamoka/controller/UserRestController.java
+++ src/main/java/br/edu/ufpel/rokamoka/controller/UserRestController.java
@@ -1,13 +1,5 @@
 package br.edu.ufpel.rokamoka.controller;
 
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.edu.ufpel.rokamoka.context.ApiResponseWrapper;
 import br.edu.ufpel.rokamoka.dto.user.input.UserAnonymousDTO;
 import br.edu.ufpel.rokamoka.dto.user.input.UserBasicDTO;
@@ -17,13 +9,16 @@ import br.edu.ufpel.rokamoka.exceptions.RokaMokaContentDuplicatedException;
 import br.edu.ufpel.rokamoka.security.AuthenticationService;
 import br.edu.ufpel.rokamoka.service.UserService;
 import br.edu.ufpel.rokamoka.wrapper.RokaMokaController;
-
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * This class is a controller for user operations. It provides endpoints
- * for creating users and logging in.
+ * This class is a controller for user operations. It provides endpoints for creating users and logging in.
  *
  * @author iyisakuma
  */
@@ -39,6 +34,7 @@ public class UserRestController extends RokaMokaController {
      * Creates a new user using the provided data.
      *
      * @param userDTO A {@link UserBasicDTO} containing the user's name, email and password.
+     *
      * @return A {@link ResponseEntity} containing the created user's JWT
      * @throws RokaMokaContentDuplicatedException if the email or name are already in use.
      */
@@ -54,10 +50,12 @@ public class UserRestController extends RokaMokaController {
             throws RokaMokaContentDuplicatedException {
         return success(this.userService.createAnonymousUser(userDTO));
     }
+
     /**
      * Authenticates a user and generates a JWT.
      *
      * @param authentication An {@link Authentication} object containing the user's credentials.
+     *
      * @return A {@link ResponseEntity} containing the user's JWT.
      */
     @PostMapping("/login")
