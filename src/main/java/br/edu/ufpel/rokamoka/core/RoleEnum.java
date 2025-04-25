c8d074eb470d280f7455d29cc5c83ba17c9d177e
iyisakuma <yujisakuma224@gmail.com>
2025-04-25 00:10:06 -0300

    adiconado o fluxo para o cadastro de usuario ser considerado um usuario comum

---
+++ src/main/java/br/edu/ufpel/rokamoka/core/RoleEnum.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 1999-2025 Touch Tecnologia e Informatica Ltda.
+ * Gomes de Carvalho, 1666, 3o. Andar, Vila Olimpia, Sao Paulo, SP, Brasil.
+ * Todos os direitos reservados.
+ *
+ * Este software e confidencial e de propriedade da Touch Tecnologia e
+ * Informatica Ltda. (Informacao Confidencial). As informacoes contidas neste
+ * arquivo nao podem ser publicadas, e seu uso esta limitado de acordo com os
+ * termos do contrato de licenca.
+ */
+
+package br.edu.ufpel.rokamoka.core;
+
+public enum RoleEnum {
+    ADMINISTRATOR, SEARCHER, CURATOR, USER
+}
