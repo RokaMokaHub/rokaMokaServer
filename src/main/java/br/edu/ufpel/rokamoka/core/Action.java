9257dc7ebda8e1b50158d5d4a87ac68f75b7815f
MauricioMucci <mauricio_mucci@hotmail.com>
2025-04-27 13:08:57 -0300

    feat(core): Add equals and hashCode overrides for entities
    Signed-off-by: MauricioMucci <mauricio_mucci@hotmail.com>

--- src/main/java/br/edu/ufpel/rokamoka/core/Action.java
+++ src/main/java/br/edu/ufpel/rokamoka/core/Action.java
@@ -1,6 +1,5 @@
 package br.edu.ufpel.rokamoka.core;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,12 +11,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
 
+import java.util.Objects;
 
 @Getter
 @Setter
 @Builder
-@ToString(of = { "name" })
+@ToString(of = {"name"})
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -27,5 +29,25 @@ public class Action {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        Action action = (Action) o;
+        return this.id != null && Objects.equals(this.id, action.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
 }
--- src/main/java/br/edu/ufpel/rokamoka/core/Role.java
+++ src/main/java/br/edu/ufpel/rokamoka/core/Role.java
@@ -1,11 +1,29 @@
 package br.edu.ufpel.rokamoka.core;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
 
-import jakarta.persistence.*;
-import lombok.*;
-
+import java.util.Objects;
 import java.util.Set;
-
 
 /**
  * A role of a user.
@@ -32,9 +50,29 @@ public class Role {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "perfil_acao",
-            joinColumns
-                    = @JoinColumn(name = "perfil_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_perfil")),
-            inverseJoinColumns
-                    = @JoinColumn(name = "acao_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_acao")))
+            joinColumns = @JoinColumn(
+                    name = "perfil_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_perfil")),
+            inverseJoinColumns = @JoinColumn(
+                    name = "acao_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_acao"))
+    )
     private Set<Action> actions;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        Role role = (Role) o;
+        return this.id != null && Objects.equals(this.id, role.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
 }
