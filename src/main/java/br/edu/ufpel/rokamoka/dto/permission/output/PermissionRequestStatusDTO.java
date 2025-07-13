2254b75388c0ae5c9229044d256a08be089fe027
JosueBorchardt <josueborchardt@gmail.com>
2025-07-13 11:17:56 -0300

    Converte a classe PermissionRequestStatusDTO de class para record

--- src/main/java/br/edu/ufpel/rokamoka/dto/permission/output/PermissionRequestStatusDTO.java
+++ src/main/java/br/edu/ufpel/rokamoka/dto/permission/output/PermissionRequestStatusDTO.java
@@ -2,17 +2,6 @@ package br.edu.ufpel.rokamoka.dto.permission.output;
 
 import br.edu.ufpel.rokamoka.core.RequestStatus;
 import br.edu.ufpel.rokamoka.core.RoleEnum;
-import lombok.Data;
 
-@Data
-public class PermissionRequestStatusDTO {
-    private long id;
-    private RequestStatus status;
-    private RoleEnum targetRole;
-
-    public PermissionRequestStatusDTO(Long id, RequestStatus status, RoleEnum targetRole) {
-        this.id = id;
-        this.status = status;
-        this.targetRole = targetRole;
-    }
+public record PermissionRequestStatusDTO(Long id, RequestStatus status, RoleEnum targetRole) {
 }
