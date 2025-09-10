b9db525b4221a0082471a58ef515cc30805ef4e5
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-09-10 16:21:42 -0300

    test(service): enhance MockRepository with default save method and ID assignment logic
    Signed-off-by: Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>

--- src/test/java/br/edu/ufpel/rokamoka/service/MockRepository.java
+++ src/test/java/br/edu/ufpel/rokamoka/service/MockRepository.java
@@ -1,10 +1,37 @@
 package br.edu.ufpel.rokamoka.service;
 
+import java.lang.reflect.Method;
+
 /**
+ * Interface for mocking repositories methods.
  *
+ * @param <T> Type of entity.
  * @author MauricioMucci
  */
 public interface MockRepository<T> {
 
-    T mockRepositorySave(T entity);
+    Long DEFAULT_ID = 1L;
+
+    default T mockRepositorySave(T entity) {
+        if (entity == null) {
+            throw new IllegalArgumentException("Entity cannot be null");
+        }
+
+        this.tryToSetId(entity);
+
+        return entity;
+    }
+
+    private void tryToSetId(T entity) {
+        try {
+            Method getId = entity.getClass().getMethod("getId");
+            Object id = getId.invoke(entity);
+            if (id == null) {
+                Method setId = entity.getClass().getMethod("setId", Long.class);
+                setId.invoke(entity, DEFAULT_ID);
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("Falha ao atribuir ID ao objeto: " + entity.getClass().getName(), e);
+        }
+    }
 }
