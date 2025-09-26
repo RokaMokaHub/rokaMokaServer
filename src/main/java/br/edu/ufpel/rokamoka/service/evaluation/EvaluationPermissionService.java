904f2833dc8c7e055470735332e9a7cdd1bd5696
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-09-26 15:17:53 -0300

    refactor(evaluation-permission-service): extract common logic into helper methods
    Signed-off-by: Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>

--- src/main/java/br/edu/ufpel/rokamoka/service/evaluation/EvaluationPermissionService.java
+++ src/main/java/br/edu/ufpel/rokamoka/service/evaluation/EvaluationPermissionService.java
@@ -24,35 +24,22 @@ public class EvaluationPermissionService implements IEvaluationPermissionService
 
     @Override
     public void deny(Long permissionId, String justificativa, String userName) throws RokaMokaContentNotFoundException, RokaMokaForbiddenException {
-        PermissionReq permissionReq = this.requestRepository.findById(permissionId).orElseThrow(RokaMokaContentNotFoundException::new);
-        if (!permissionReq.isPending()) {
-            throw new RokaMokaForbiddenException("Não é possível negar pedido que já foi analisado");
-        }
+        PermissionReq permissionReq = this.getPendingPermissionRequestOrElseThrow(permissionId);
+
         permissionReq.setStatus(RequestStatus.DENY);
         permissionReq = this.requestRepository.save(permissionReq);
 
-        PermissionReg register = PermissionReg.builder()
-                .justification(justificativa)
-                .reviewer(this.userService.getByNome(userName))
-                .request(permissionReq)
-                .build();
-        this.registerRepository.save(register);
+        this.createPermissionRegistration(justificativa, userName, permissionReq);
     }
 
     @Override
     public void accept(Long permissionId, String userName) throws RokaMokaContentNotFoundException, RokaMokaForbiddenException {
-        PermissionReq permissionReq = this.requestRepository.findById(permissionId).orElseThrow(RokaMokaContentNotFoundException::new);
-        if (!permissionReq.isPending()) {
-            throw new RokaMokaForbiddenException("Não é possível aceitar pedido que já foi analisado");
-        }
+        PermissionReq permissionReq = this.getPendingPermissionRequestOrElseThrow(permissionId);
+
         permissionReq.setStatus(RequestStatus.CONFIRM);
         permissionReq = this.requestRepository.save(permissionReq);
-        PermissionReg register = PermissionReg.builder()
-                .justification("")
-                .reviewer(this.userService.getByNome(userName))
-                .request(permissionReq)
-                .build();
-        this.registerRepository.save(register);
+
+        this.createPermissionRegistration("", userName, permissionReq);
 
         this.userService.updateRole(permissionReq.getRequester(), permissionReq.getTargetRole());
     }
@@ -60,5 +47,28 @@ public class EvaluationPermissionService implements IEvaluationPermissionService
     @Override
     public List<RequestDetailsDTO> findAllPendingRequest() {
         return this.requestRepository.findAllPendingRequestDetailed();
+    }
+
+    private PermissionReq getPendingPermissionRequestOrElseThrow(Long permissionId)
+    throws RokaMokaContentNotFoundException, RokaMokaForbiddenException {
+        PermissionReq permissionReq = this.getRequestOrElseThrow(permissionId);
+        if (!permissionReq.isPending()) {
+            throw new RokaMokaForbiddenException("Não é possível aceitar/negar pedido que já foi analisado");
+        }
+        return permissionReq;
+    }
+
+    private PermissionReq getRequestOrElseThrow(Long permissionId) throws RokaMokaContentNotFoundException {
+        return this.requestRepository.findById(permissionId).orElseThrow(RokaMokaContentNotFoundException::new);
+    }
+
+    private PermissionReg createPermissionRegistration(String justificativa, String userName, PermissionReq permissionReq)
+    throws RokaMokaContentNotFoundException {
+        PermissionReg register = PermissionReg.builder()
+                .justification(justificativa)
+                .reviewer(this.userService.getByNome(userName))
+                .request(permissionReq)
+                .build();
+        return this.registerRepository.save(register);
     }
 }
