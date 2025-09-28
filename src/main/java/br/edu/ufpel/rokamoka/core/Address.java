cf9d812a6844a00e5759a78424ad0a195407b250
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-09-28 16:34:36 -0300

    feat(core): update `Address` entity with Hibernate mappings and enhanced equality
    Signed-off-by: Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>

--- src/main/java/br/edu/ufpel/rokamoka/core/Address.java
+++ src/main/java/br/edu/ufpel/rokamoka/core/Address.java
@@ -1,8 +1,21 @@
 package br.edu.ufpel.rokamoka.core;
 
-import br.edu.ufpel.rokamoka.dto.address.input.EnderecoDTO;
-import jakarta.persistence.*;
-import lombok.*;
+import br.edu.ufpel.rokamoka.dto.location.input.AddressInputDTO;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -12,18 +25,48 @@ import lombok.*;
 @Entity
 @Table(name = "endereco")
 public class Address {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String rua;
-    private String numero;
-    private String cep;
+
+    @Column(nullable = false) private String rua;
+
+    @Column(nullable = false) private String numero;
+
+    @Column(nullable = false) private String cep;
+
     private String complemento;
 
-    public Address(EnderecoDTO enderecoDTO) {
-        this.cep = enderecoDTO.cep();
-        this.complemento = enderecoDTO.complemento();
-        this.rua = enderecoDTO.rua();
-        this.numero = enderecoDTO.numero();
+    public Address(AddressInputDTO addressInputDTO) {
+        this.cep = addressInputDTO.cep();
+        this.complemento = addressInputDTO.complemento();
+        this.rua = addressInputDTO.rua();
+        this.numero = addressInputDTO.numero();
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        Address other = (Address) o;
+        if (this.id != null && Objects.equals(this.id, other.getId())) {
+            return true;
+        }
+        return this.rua != null && this.numero != null && this.cep != null &&
+               Objects.equals(this.rua, other.rua) &&
+               Objects.equals(this.numero, other.numero) &&
+               Objects.equals(this.cep, other.cep);
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                .getPersistentClass()
+                .hashCode() : getClass().hashCode();
     }
 }
