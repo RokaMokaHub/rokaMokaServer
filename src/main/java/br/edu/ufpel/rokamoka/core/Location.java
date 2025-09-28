c3cfa5690f7469b105d177cd649ac2a4997977bb
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-09-28 16:01:45 -0300

    feat(core): add `Location` entity with Hibernate mappings
    Signed-off-by: Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>

---
+++ src/main/java/br/edu/ufpel/rokamoka/core/Location.java
@@ -0,0 +1,65 @@
+package br.edu.ufpel.rokamoka.core;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "local")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 255)
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @OneToOne(cascade = CascadeType.REMOVE, optional = false)
+    @JoinColumn(name = "endereco_id", nullable = false)
+    private Address endereco;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        Location other = (Location) o;
+        if (this.id != null && Objects.equals(this.id, other.getId())) {
+            return true;
+        }
+        return this.nome != null && Objects.equals(this.nome, other.getNome());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
+}
