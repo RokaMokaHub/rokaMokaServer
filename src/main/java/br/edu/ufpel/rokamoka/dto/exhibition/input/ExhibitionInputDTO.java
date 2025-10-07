4590a0636e2f760a225d98d5512342c5cc434116
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-10-07 14:47:26 -0300

    feat(exhibition-dto): enhance DTOs with additional fields and validations

--- src/main/java/br/edu/ufpel/rokamoka/dto/exhibition/input/ExhibitionInputDTO.java
+++ src/main/java/br/edu/ufpel/rokamoka/dto/exhibition/input/ExhibitionInputDTO.java
@@ -1,6 +1,25 @@
 package br.edu.ufpel.rokamoka.dto.exhibition.input;
 
-import br.edu.ufpel.rokamoka.dto.location.input.AddressInputDTO;
+import br.edu.ufpel.rokamoka.dto.GroupValidators;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 
-public record ExhibitionInputDTO(String name, String description, AddressInputDTO addressInputDTO) {
-}
+/**
+ * A Data Transfer Object (DTO) representing the input data of a exhibition.
+ *
+ * @param id The unique identifier of the exhibition.
+ * @param name The name of the exhibition.
+ * @param description The description of the exhibition.
+ * @param locationId The unique identifier of the place where the exhibition is held.
+ *
+ * @author MauricioMucci
+ * @see br.edu.ufpel.rokamoka.core.Exhibition
+ */
+public record ExhibitionInputDTO(@NotNull(groups = GroupValidators.Update.class,
+        message = "O ID da exposição é obrigatório durante a atualização") @Null(groups = GroupValidators.Create.class,
+        message = "O ID da exposição não pode ser fornecido durante a criação") Long id,
+                                 @NotBlank(message = "O nome da exposição é obrigatório") String name,
+                                 String description,
+                                 @NotNull(groups = GroupValidators.Create.class,
+                                         message = "O ID do local é obrigatório durante a criação") Long locationId) {}
--- src/main/java/br/edu/ufpel/rokamoka/dto/exhibition/output/ExhibitionWithArtworksDTO.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ufpel.rokamoka.dto.exhibition.output;
-
-import br.edu.ufpel.rokamoka.dto.artwork.output.ArtworkOutputDTO;
-
-import java.util.List;
-
-public record ExhibitionWithArtworksDTO(String exhibitionName, List<ArtworkOutputDTO> artworkOutputDTOS) {
-}
