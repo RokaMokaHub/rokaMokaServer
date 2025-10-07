35f4f1fe4ccf111bcaa610948d8524783c679ad8
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-10-07 14:40:31 -0300

    feat(exhibition): enhance CRUD operations and align with updated schema

--- src/main/java/br/edu/ufpel/rokamoka/controller/ExhibitionController.java
+++ src/main/java/br/edu/ufpel/rokamoka/controller/ExhibitionController.java
@@ -1,18 +1,20 @@
 package br.edu.ufpel.rokamoka.controller;
 
 import br.edu.ufpel.rokamoka.context.ApiResponseWrapper;
-import br.edu.ufpel.rokamoka.core.Exhibition;
+import br.edu.ufpel.rokamoka.dto.GroupValidators;
 import br.edu.ufpel.rokamoka.dto.artwork.input.ArtworkInputDTO;
 import br.edu.ufpel.rokamoka.dto.exhibition.input.ExhibitionInputDTO;
 import br.edu.ufpel.rokamoka.dto.exhibition.output.ExhibitionOutputDTO;
-import br.edu.ufpel.rokamoka.dto.exhibition.output.ExhibitionWithArtworksDTO;
 import br.edu.ufpel.rokamoka.exceptions.RokaMokaContentNotFoundException;
 import br.edu.ufpel.rokamoka.service.exhibition.IExhibitionService;
 import br.edu.ufpel.rokamoka.wrapper.RokaMokaController;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Tag(name = "Exibição", description = "API para operações de CRUD em exposições")
+@Validated
 @RestController
 @RequestMapping("/exhibition")
 @RequiredArgsConstructor
@@ -31,35 +34,51 @@ public class ExhibitionController extends RokaMokaController {
 
     private final IExhibitionService exhibitionService;
 
-    @Operation(summary = "Buscar exibição por ID", description = "Retorna uma exibição com base no ID informado")
+    @Operation(summary = "Buscar exposição por id", description = "Retorna uma exposição com base no ID informado")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Exposição encontrada"),
+            @ApiResponse(responseCode = "404", description = "Exposição não encontrada")})
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponseWrapper<ExhibitionOutputDTO>> findById(@PathVariable Long id)
+    public ResponseEntity<ApiResponseWrapper<ExhibitionOutputDTO>> getExhibition(@PathVariable Long id)
     throws RokaMokaContentNotFoundException {
-        Exhibition exhibition = this.exhibitionService.findById(id);
-        return this.success(new ExhibitionOutputDTO(exhibition));
+        ExhibitionOutputDTO exhibition = this.exhibitionService.findById(id);
+        return this.success(exhibition);
     }
 
-    @Operation(summary = "Criar nova exibição", description = "Cria uma nova exibição com os dados fornecidos")
+    @Operation(summary = "Buscar todos as exposições",
+            description = "Retorna uma lista contendo todos as exposições do sistema")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Lista de todas as exposições")})
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponseWrapper<List<ExhibitionOutputDTO>>> getAllExhibitions() {
+        List<ExhibitionOutputDTO> exhibitions = this.exhibitionService.getAllExhibitions();
+        return this.success(exhibitions);
+    }
+
+    @Operation(summary = "Cadastrar nova exposição",
+            description = "Cadastra uma nova exposição com os dados fornecidos")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Exposição cadastrada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Exposição duplicada"),
+            @ApiResponse(responseCode = "500", description = "Erro inesperado ao cadastrar exposição")})
     @PostMapping
-    public ResponseEntity<ApiResponseWrapper<ExhibitionOutputDTO>> create(@RequestBody ExhibitionInputDTO dto) {
-        Exhibition exhibition = this.exhibitionService.save(dto);
-        return this.success(new ExhibitionOutputDTO(exhibition));
+    public ResponseEntity<ApiResponseWrapper<ExhibitionOutputDTO>> register(
+            @RequestBody @Validated(value = GroupValidators.Create.class) ExhibitionInputDTO dto)
+    throws RokaMokaContentNotFoundException {
+        ExhibitionOutputDTO exhibition = this.exhibitionService.create(dto);
+        return this.success(exhibition);
     }
 
-    @Operation(summary = "Atualizar exibição", description = "Atualiza os dados de uma exibição existente")
+    @Operation(summary = "Atualizar obras da exposição", description = "Atualiza os dados de uma exposição existente")
     @PostMapping("add/artwork/{id}")
-    public ResponseEntity<ApiResponseWrapper<ExhibitionWithArtworksDTO>> addArtworks(@PathVariable Long id,
-            @RequestBody List<ArtworkInputDTO> artworkInputDTOS) throws RokaMokaContentNotFoundException {
-        ExhibitionWithArtworksDTO dto = this.exhibitionService.addArtworks(id, artworkInputDTOS);
-        return this.success(dto);
+    public ResponseEntity<ApiResponseWrapper<ExhibitionOutputDTO>> addArtworks(@PathVariable Long id,
+            @RequestBody List<ArtworkInputDTO> artworks) throws RokaMokaContentNotFoundException {
+        ExhibitionOutputDTO exhibition = this.exhibitionService.addArtworks(id, artworks);
+        return this.success(exhibition);
     }
 
     @Operation(summary = "Deletar exibição", description = "Remove uma exibição com base no ID informado")
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponseWrapper<ExhibitionOutputDTO>> delete(@PathVariable Long id)
+    public ResponseEntity<ApiResponseWrapper<ExhibitionOutputDTO>> remove(@PathVariable Long id)
     throws RokaMokaContentNotFoundException {
-        Exhibition exhibition = this.exhibitionService.findById(id);
-        this.exhibitionService.deleteById(id);
-        return this.success(new ExhibitionOutputDTO(exhibition));
+        ExhibitionOutputDTO exhibition = this.exhibitionService.delete(id);
+        return this.success(exhibition);
     }
 }
--- src/main/java/br/edu/ufpel/rokamoka/service/exhibition/ExhibitionService.java
+++ src/main/java/br/edu/ufpel/rokamoka/service/exhibition/ExhibitionService.java
@@ -1,18 +1,19 @@
 package br.edu.ufpel.rokamoka.service.exhibition;
 
-import br.edu.ufpel.rokamoka.core.Address;
 import br.edu.ufpel.rokamoka.core.Artwork;
 import br.edu.ufpel.rokamoka.core.Exhibition;
+import br.edu.ufpel.rokamoka.core.Location;
 import br.edu.ufpel.rokamoka.dto.artwork.input.ArtworkInputDTO;
 import br.edu.ufpel.rokamoka.dto.artwork.output.ArtworkOutputDTO;
 import br.edu.ufpel.rokamoka.dto.exhibition.input.ExhibitionInputDTO;
-import br.edu.ufpel.rokamoka.dto.exhibition.output.ExhibitionWithArtworksDTO;
+import br.edu.ufpel.rokamoka.dto.exhibition.output.ExhibitionOutputDTO;
 import br.edu.ufpel.rokamoka.exceptions.RokaMokaContentNotFoundException;
-import br.edu.ufpel.rokamoka.repository.AddressRepository;
-import br.edu.ufpel.rokamoka.repository.ArtworkRepository;
 import br.edu.ufpel.rokamoka.repository.ExhibitionRepository;
+import br.edu.ufpel.rokamoka.service.artwork.IArtworkService;
+import br.edu.ufpel.rokamoka.service.location.ILocationService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,53 +22,63 @@ import java.util.List;
 public class ExhibitionService implements IExhibitionService {
 
     private final ExhibitionRepository exhibitionRepository;
-    private final ArtworkRepository artworkRepository;
-    private final AddressRepository addressRepository;
+    private final IArtworkService artworkService;
+    private final ILocationService locationService;
 
     @Override
-    public List<Exhibition> findAll() {
-        return this.exhibitionRepository.findAll();
+    public List<ExhibitionOutputDTO> getAllExhibitions() {
+        return this.exhibitionRepository.findAllExhibitionAndCountArtworks();
     }
 
     @Override
-    public Exhibition findById(Long id) throws RokaMokaContentNotFoundException {
-        return this.getOrElseThrow(id);
+    public ExhibitionOutputDTO findById(Long id) throws RokaMokaContentNotFoundException {
+        Exhibition exhibition = this.getExhibitionOrElseThrow(id);
+
+        this.loadArtworks(exhibition);
+
+        return new ExhibitionOutputDTO(exhibition);
     }
 
-    private Exhibition getOrElseThrow(Long exhibitionId) throws RokaMokaContentNotFoundException {
-        return this.exhibitionRepository.findById(exhibitionId).orElseThrow(RokaMokaContentNotFoundException::new);
+    private void loadArtworks(Exhibition exhibition) {
+        List<Artwork> artworks = this.artworkService.getAllArtworkByExhibitionId(exhibition.getId());
+        exhibition.setArtworks(artworks);
     }
 
     @Override
-    public Exhibition save(ExhibitionInputDTO dto) {
+    @Transactional
+    public ExhibitionOutputDTO create(ExhibitionInputDTO dto) throws RokaMokaContentNotFoundException {
+        Location location = this.locationService.getLocationOrElseThrow(dto.locationId());
         Exhibition exhibition = Exhibition.builder()
                 .name(dto.name())
                 .description(dto.description())
-                .address(this.addressRepository.save(new Address(dto.addressInputDTO())))
+                .location(location)
                 .build();
-        return this.exhibitionRepository.save(exhibition);
+        exhibition = this.exhibitionRepository.save(exhibition);
+        return new ExhibitionOutputDTO(exhibition);
     }
 
     @Override
-    public void deleteById(Long id) {
-        this.exhibitionRepository.deleteById(id);
+    public ExhibitionOutputDTO delete(Long id) throws RokaMokaContentNotFoundException {
+        Exhibition exhibition = this.getExhibitionOrElseThrow(id);
+
+        List<ArtworkOutputDTO> artworks = this.artworkService.deleteByExhibitionId(exhibition.getId());
+
+        this.exhibitionRepository.delete(exhibition);
+        return new ExhibitionOutputDTO(exhibition);
     }
 
     @Override
-    public ExhibitionWithArtworksDTO addArtworks(Long exhibitionId, List<ArtworkInputDTO> artworkInputDTOS)
+    @Transactional
+    public ExhibitionOutputDTO addArtworks(Long id, List<ArtworkInputDTO> inputList)
     throws RokaMokaContentNotFoundException {
-        Exhibition exhibition = this.getOrElseThrow(exhibitionId);
-        List<ArtworkOutputDTO> artworks = this.createArtworksForExhibition(exhibition, artworkInputDTOS);
-        return new ExhibitionWithArtworksDTO(exhibition.getName(), artworks);
+        Exhibition exhibition = this.getExhibitionOrElseThrow(id);
+        List<ArtworkOutputDTO> artworks = this.artworkService.addArtworksToExhibition(inputList, exhibition);
+        return new ExhibitionOutputDTO(exhibition, (long) artworks.size());
     }
 
-    private List<ArtworkOutputDTO> createArtworksForExhibition(Exhibition exhibition, List<ArtworkInputDTO> artworkInputDTOS) {
-        List<Artwork> artworks = artworkInputDTOS.stream()
-                .map(a -> new Artwork(a, exhibition))
-                .toList();
-        return this.artworkRepository.saveAll(artworks)
-                .stream()
-                .map(ArtworkOutputDTO::new)
-                .toList();
+    @Override
+    public Exhibition getExhibitionOrElseThrow(Long id) throws RokaMokaContentNotFoundException {
+        return this.exhibitionRepository.findById(id).orElseThrow(() ->
+                new RokaMokaContentNotFoundException("Exposição não encontrada"));
     }
 }
