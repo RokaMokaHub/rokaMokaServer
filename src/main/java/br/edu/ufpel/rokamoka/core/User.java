3de3bc001350a332143100e38d8d510c09f69878
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-10-18 15:36:03 -0300

    feat(user): add first/last name

--- src/main/java/br/edu/ufpel/rokamoka/core/User.java
+++ src/main/java/br/edu/ufpel/rokamoka/core/User.java
@@ -47,6 +47,9 @@ public class User extends Auditable {
 
     @Column(nullable = false, unique = true) private String nome;
 
+    @Column(name = "first_name") private String firstName;
+    @Column(name = "last_name") private String lastName;
+
     @Column(nullable = false) private String senha;
 
     @ManyToOne(fetch = FetchType.EAGER)
