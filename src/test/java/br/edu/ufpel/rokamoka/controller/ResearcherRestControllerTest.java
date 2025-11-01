625d96c5ead9e40ad71286d9e218574daa07891a
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-11-01 14:41:59 -0300

    refactor(researcher-controller): remove redundant `throws` for runtime exceptions
    Signed-off-by: Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>

---
+++ src/test/java/br/edu/ufpel/rokamoka/controller/ResearcherRestControllerTest.java
@@ -0,0 +1,54 @@
+package br.edu.ufpel.rokamoka.controller;
+
+import br.edu.ufpel.rokamoka.context.ApiResponseWrapper;
+import br.edu.ufpel.rokamoka.dto.user.input.UserBasicDTO;
+import br.edu.ufpel.rokamoka.dto.user.output.UserAuthDTO;
+import br.edu.ufpel.rokamoka.service.user.UserService;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit tests for the {@link ResearcherRestController} class, which is responsible for handling researcher-related
+ * endpoints.
+ *
+ * @author MauricioMucci
+ * @see ResearcherRestController
+ * @see UserService
+ */
+@ExtendWith(MockitoExtension.class)
+class ResearcherRestControllerTest implements ControllerResponseValidator {
+
+    @InjectMocks private ResearcherRestController researcherController;
+
+    @Mock private UserService userService;
+
+    //region register
+    @Test
+    void register_shouldReturnTokenGeneratedForNewUser_whenSuccessful() {
+        // Arrange
+        UserAuthDTO expectedOutput = Instancio.create(UserAuthDTO.class);
+
+        when(this.userService.createResearcher(any(UserBasicDTO.class))).thenReturn(expectedOutput);
+
+        // Act
+        ResponseEntity<ApiResponseWrapper<UserAuthDTO>> response = this.researcherController.register(
+                mock(UserBasicDTO.class));
+
+        // Assert
+        this.assertExpectedResponse(response, expectedOutput);
+
+        verify(this.userService, times(1)).createResearcher(any(UserBasicDTO.class));
+    }
+    //endregion
+}
