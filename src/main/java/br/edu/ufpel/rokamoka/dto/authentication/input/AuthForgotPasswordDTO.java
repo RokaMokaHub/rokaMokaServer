bcab255e8032399b4e9b899922786e41f31e9c1f
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-12-13 17:07:06 -0300

    refactor(user-service): update password reset flow to use JWT validation
    - Replaced username and email validation with JWT token-based authentication in the `resetUserPassword` method.
    - Removed `getForgotPasswordUser` method.
    - Updated `AuthForgotPasswordDTO` to include token instead of username and email.

--- src/main/java/br/edu/ufpel/rokamoka/dto/authentication/input/AuthForgotPasswordDTO.java
+++ src/main/java/br/edu/ufpel/rokamoka/dto/authentication/input/AuthForgotPasswordDTO.java
@@ -1,21 +1,16 @@
 package br.edu.ufpel.rokamoka.dto.authentication.input;
 
-import br.edu.ufpel.rokamoka.utils.user.EmailConstraint;
 import br.edu.ufpel.rokamoka.utils.user.PasswordConstraint;
-import br.edu.ufpel.rokamoka.utils.user.UserNameConstraint;
+import jakarta.validation.constraints.NotBlank;
 
 /**
  * A Data Transfer Object (DTO) for handling user password recovery requests.
  *
- * @param email The user's email address.
- * @param name The user's username.
+ * @param token The user's token for password recovery.
  * @param newPassword The user's new password.
  *
  * @author MauricioMucci
  * @see AuthForgotPasswordDTO
- * @see EmailConstraint
- * @see UserNameConstraint
  * @see PasswordConstraint
  */
-public record AuthForgotPasswordDTO(
-        @EmailConstraint String email, @UserNameConstraint String name, @PasswordConstraint String newPassword) {}
+public record AuthForgotPasswordDTO(@NotBlank String token, @PasswordConstraint String newPassword) {}
