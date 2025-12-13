d6ca2c426edd4c7b3d76156275e5bce83c120377
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-12-13 17:02:46 -0300

    feat(security): implement token decoding and enhance token generation

--- src/main/java/br/edu/ufpel/rokamoka/security/JwtService.java
+++ src/main/java/br/edu/ufpel/rokamoka/security/JwtService.java
@@ -3,7 +3,9 @@ package br.edu.ufpel.rokamoka.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,30 @@ public class JwtService {
     private static final String ROKAMOKA = "ROKAMOKA";
 
     private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+
+    public String getSubject(String token) {
+        Jwt jwt = this.jwtDecoder.decode(token);
+        return jwt.getSubject();
+    }
+
+    public String generateToken(UserAuthenticated user) {
+        Instant now = Instant.now();
+
+        String scope = user.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet claims = JwtClaimsSet.builder().issuer(ROKAMOKA)
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(900L)) // 15 minutes
+                .subject(user.getUsername())
+                .claim("scope", scope)
+                .build();
+
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
 
     /**
      * Generates a JWT token given an {@link Authentication} object.
@@ -42,7 +68,6 @@ public class JwtService {
      */
     public String generateToken(Authentication authentication) {
         Instant now = Instant.now();
-        long expiry = 36000000L; // 10 hours in milliseconds
 
         String scope = authentication.getAuthorities()
                 .stream()
@@ -51,7 +76,7 @@ public class JwtService {
 
         JwtClaimsSet claims = JwtClaimsSet.builder().issuer(ROKAMOKA)
                 .issuedAt(now)
-                .expiresAt(now.plusSeconds(expiry))
+                .expiresAt(now.plusSeconds(36000L)) // 10 hours
                 .subject(authentication.getName())
                 .claim("scope", scope)
                 .build();
