bb30edf79ad250887eafda510eaafd6119d8be18
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2026-03-28 17:04:27 -0300

    fix(image): import the same library for nullable annotations

--- src/main/java/br/edu/ufpel/rokamoka/service/image/ImageService.java
+++ src/main/java/br/edu/ufpel/rokamoka/service/image/ImageService.java
@@ -2,12 +2,13 @@ package br.edu.ufpel.rokamoka.service.image;
 
 import br.edu.ufpel.rokamoka.core.Image;
 import br.edu.ufpel.rokamoka.repository.ImageRepository;
-import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
@@ -26,7 +27,7 @@ public class ImageService implements IIMageService {
             var imageSize = image.getSize();
             var imageBytes = image.getBytes();
             var savedImage = this.imageRepository.save(new Image(imageBytes, imageSize));
-            return Set.of(savedImage);
+            return new HashSet<>(Set.of(savedImage));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
