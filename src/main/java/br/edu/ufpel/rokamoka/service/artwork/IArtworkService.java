cc91d71fcef737f817efe524ba4d78d21981f1bc
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2026-06-07 12:57:45 -0300

    feat(service): allow artwork editing for researchers with emblem validations

--- src/main/java/br/edu/ufpel/rokamoka/service/artwork/IArtworkService.java
+++ src/main/java/br/edu/ufpel/rokamoka/service/artwork/IArtworkService.java
@@ -4,28 +4,24 @@ import br.edu.ufpel.rokamoka.core.Artwork;
 import br.edu.ufpel.rokamoka.core.Exhibition;
 import br.edu.ufpel.rokamoka.dto.artwork.input.ArtworkInputDTO;
 import br.edu.ufpel.rokamoka.dto.artwork.output.ArtworkOutputDTO;
-import jakarta.validation.Valid;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IArtworkService {
 
+    Artwork create(Long exhibitionId, ArtworkInputDTO artworkInputDTO);
+
+    Artwork update(ArtworkInputDTO input);
+
+    ArtworkOutputDTO delete(Long id);
+
     Artwork getArtworkOrElseThrow(Long id);
 
-    Optional<Artwork> findByQrCode(String qrCode);
-
     Artwork getByQrCodeOrThrow(String qrCode);
-
-    Artwork create(Long exhibitionId, @Valid ArtworkInputDTO artworkInputDTO);
 
     List<Artwork> getAllArtworkByExhibitionId(Long exhibitionId);
 
     List<ArtworkOutputDTO> addArtworksToExhibition(List<ArtworkInputDTO> inputList, Exhibition exhibition);
 
     List<ArtworkOutputDTO> deleteByExhibitionId(Long exhibitionId);
-
-    ArtworkOutputDTO update(ArtworkInputDTO input);
-
-    ArtworkOutputDTO delete(Long id);
 }
